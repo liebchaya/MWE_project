7878b0662e309b10f40465157f5438563a813f45
avitalDay <avitalush1995@gmail.com>
2016-01-14 02:31:25 +0200

    addTest

--- src/test/TestIndexInfo.java
+++ src/test/TestIndexInfo.java
@@ -1,6 +1,12 @@
 package test;
 
+import java.awt.List;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+import models.MweExample;
+import morphologyTools.Tagger;
 import searcher.IndexInfo;
 
 public class TestIndexInfo {
@@ -9,12 +15,17 @@ public class TestIndexInfo {
 	 * @param args
 	 * @throws Exception 
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		IndexInfo indexInfo = new IndexInfo("C:\\Users\\aday\\AppData\\Local\\GitHub\\TutorialRepository_a66c3719071da6d865a984bb8d6bfb5bcd775ec8\\new-repo\\MWE_project\\allMila");
-		System.out.println("Number of sentences: " + indexInfo.getIndexSize());
-		System.out.println("Sentence 678: " + indexInfo.getSentenceContent(678));
-		System.out.println("Sentence 6780: " + indexInfo.getSentenceContent(6780));
-
+		Tagger.init("c:\\BGUTaggerData\\");
+		
+//		System.out.println("Number of sentences: " + indexInfo.getIndexSize());
+//		System.out.println("Sentence 678: " + indexInfo.getSentenceContent(678));
+//		System.out.println("Sentence 6780: " + indexInfo.getSentenceContent(6780));
+		
+		Iterable<MweExample> m =indexInfo.GenerateNegativeExamples(indexInfo.randomizeSentences(600, 10));
+		for(MweExample mweExample : m)
+			System.out.println(mweExample.getMwe());
 	}
 
 }
